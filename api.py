69315f3ab924101f2d7c922bf2a1ff8bb577a9dc
saicharanruka <saicharanruka@gmail.com>
2025-11-14 12:50:28 +0530

    added users and user resource

--- api.py
+++ api.py
@@ -1,10 +1,65 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+api = Api(app)
 db = SQLAlchemy(app)
+
+
+class UserModel(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True)
+    username  = db.Column(db.String(80), unique=True, nullable=False)
+    email  = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User(name= {self.name}, email={self.email})"
+
+user_args = reqparse.RequestParser()
+user_args.add_argument("username", type=str, required=True, help="Username cannot be blank")
+user_args.add_argument("email", type=str, required=True, help="Email cannot be blank")
+
+userFields = {
+    'id': fields.Integer,
+    'username': fields.String,
+    'email': fields.String,
+}
+
+
+class Users(Resource):
+    @marshal_with(userFields)
+    def get(self):
+        users = UserModel.query.all()
+        return users
+    
+
+    @marshal_with(userFields)
+    def post(self):
+        args = user_args.parse_args()
+        user = UserModel(username=args["username"], email=args["email"])
+        db.session.add(user)
+        db.session.commit()
+
+        users = UserModel.query.all()
+        return users, 201
+
+class User(Resource):
+    @marshal_with(userFields)
+    def get(self, id):
+        user = UserModel.query.filter_by(id=id).first()
+
+        if not user:
+            abort(404, "User not found")
+        return user
+
+
+api.add_resource(Users, '/api/users/')
+api.add_resource(User, '/api/user/<int:id>')
 
 
 
